e7231ce48de4dde704969e603b5da5b4d94e746b
Ingo Reitz <ninelore@protonmail.com>
2019-10-30 05:40:07 +0100

    Update 9l-i3-install.sh
    removed --noconfirm pacman option

--- 9l-i3-install.sh
+++ 9l-i3-install.sh
@@ -1,7 +1,7 @@
 echo "ninelore's dotfiles installation script 0v1"
 echo
 echo "Installing Dependencies"
-yay -S --needed --noconfirm arc-gtk-theme arc-kde calc compton dmenu2 feh firefox firefox-tridactyl htop i3-gaps i3blocks i3lock i3status lxappearance network-manager-applet networkmanager neofetch nitrogen noto-fonts noto-fonts-emoji numlockx qt5ct ranger rxvt-unicode tamzen-font ttf-font-awesome ttf-font-icons vim volumeicon xautolock xcursor-breeze xfce4-power-manager xorg-xinit zsh zsh-lovers 
+yay -S --needed arc-gtk-theme arc-kde calc compton dmenu2 feh firefox firefox-tridactyl htop i3-gaps i3blocks i3lock i3status lxappearance network-manager-applet networkmanager neofetch nitrogen noto-fonts noto-fonts-emoji numlockx qt5ct ranger rxvt-unicode tamzen-font ttf-font-awesome ttf-font-icons vim volumeicon xautolock xcursor-breeze xfce4-power-manager xorg-xinit zsh zsh-lovers 
 echo
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
