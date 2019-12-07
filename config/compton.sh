59595669103e4be11c0deb6c3e85e45f1d536e0e
Ingo Reitz <ninelore@protonmail.com>
2019-12-07 14:02:35 +0100

    Hotfix for the huge Update

--- config/compton.sh
+++ config/compton.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
-
-# Terminate already running bar instances
-killall -q compton
-
-# Wait until the processes have been shut down
-while pgrep -x compton >/dev/null; do sleep 1; done
-
-# Launch compton
-compton --config  ~/.config/compton.conf -b &
+if pgrep -x "compton" > /dev/null
+then
+	killall compton
+else
+	compton --config  ~/.config/compton.conf -b
+fi
